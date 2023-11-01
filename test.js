1aaab1874394ee7a6f966b4f0437dfe333990b54
“mohammadfarhadfard” <mohammadfarhadfard@gmail.com>
2023-11-02 01:15:40 +0330

    set api via express in app-api.js and some change in test

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 const axios = require('axios');
-const { response } = require('express');
+
 
 axios.get('https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=BTC-USDT')
   .then(response => {
     console.log(response.data.data.price);
     let prices = response.data.data.price/2;
-
   })
   .catch(error => {
     console.log(error);
