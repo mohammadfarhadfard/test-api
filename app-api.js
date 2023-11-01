b93b44cfb33b7f41c459da3a37445f7f59d4e6f1
“mohammadfarhadfard” <mohammadfarhadfard@gmail.com>
2023-11-02 01:19:08 +0330

    some change in app-api

--- app-api.js
+++ app-api.js
@@ -3,12 +3,10 @@ const https = require('https');
 https.get('https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=BTC-USDT', (resp) => {
   let data = '';
 
-  // A chunk of data has been received.
   resp.on('data', (chunk) => {
     data += chunk;
   });
 
-  // The whole response has been received. Print out the result.
   resp.on('end', () => {
     console.log(JSON.parse(data));
   });
